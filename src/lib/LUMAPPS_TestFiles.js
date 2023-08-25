1e79946791328f0468740dd76c81e3ff39b4693b
Eudes Boullie <eudes@lumapps.com>
2023-08-25 09:48:42 +0200

    feat(box content preview): add lummaps test variable file EIN-2249

---
+++ src/lib/LUMAPPS_TestFiles.js
@@ -0,0 +1,10 @@
+const contextTest = {
+    lang: 'en',
+    token: 'YOUR_LUMAPPS_TOKEN',
+    baseUrl: 'YOUR_BASE_URL',
+    organizationId: 'YOUR_ORGANISATION_ID',
+    haussmannCell: 'YOUR_HAUSSMANN_CELL',
+};
+const connectorTest = 'YOUR_LUMAPPS_CONNECTOR_ID';
+
+export { connectorTest, contextTest };
